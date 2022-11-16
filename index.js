bb33f2565daaded2c04183637f86b60bf3c7af1d
anisurarzu <anisur35-2163@diu.edu.bd>
2022-11-16 12:28:29 +0600

    routes add

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
+require("./index.js");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g7zap.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
